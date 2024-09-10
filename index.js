fd37c3282009816571dc8f0ec030c25574983363
BrunosPadilha <padilha_bruno@outlook.com>
2024-09-09 23:17:12 -0300

    fundamentos 002

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+        value: 'ler um livro todo mês',
+        checked: true,
+} 
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
